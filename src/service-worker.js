fcb0fd0a3bbbddf41a1271e99315505afc4be171
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-12-09 19:04:00 -0600

    u

--- src/service-worker.js
+++ src/service-worker.js
@@ -57,7 +57,6 @@
 
 
 
-
 const CACHE_NAME = "v2"; // Versión del cache
 const CACHE_URLS = [
   "/",
@@ -69,12 +68,17 @@ const CACHE_URLS = [
 ];
 
 self.addEventListener("install", (event) => {
+  console.log("Service Worker: Install event triggered");
   const newVersion = "v2"; // Define manualmente la versión nueva
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker: Caching files");
       return cache.addAll(CACHE_URLS).then(() => {
         // Notificar a los clientes sobre la nueva versión
         self.clients.matchAll().then((clients) => {
+          console.log(
+            "Service Worker: Sending UPDATE_READY message to clients"
+          );
           clients.forEach((client) => {
             client.postMessage({ type: "UPDATE_READY", newVersion });
           });
@@ -84,20 +88,23 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Escuchar el mensaje de 'SKIP_WAITING' para permitir la actualización manualmente
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
+    console.log("Service Worker: SKIP_WAITING received, skipping waiting");
     self.skipWaiting(); // Ejecutar solo si el cliente lo solicita
   }
 });
 
 self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activate event triggered");
   // Borrar cachés antiguos si la versión del cache ha cambiado
   event.waitUntil(
     caches.keys().then((cacheNames) => {
+      console.log("Service Worker: Cleaning old caches");
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
+            console.log(`Service Worker: Deleting cache ${cacheName}`);
             return caches.delete(cacheName);
           }
         })
@@ -107,17 +114,20 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  console.log(`Service Worker: Fetch event for ${event.request.url}`);
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Si encontramos una respuesta en el cache, la usamos
       if (cachedResponse) {
+        console.log("Service Worker: Returning cached response");
         return cachedResponse;
       }
-      // Si no, la buscamos en la red
+      console.log("Service Worker: Fetching from network");
       return fetch(event.request).then((response) => {
-        // Cachear la nueva respuesta si es una solicitud de tipo GET
         if (event.request.method === "GET") {
           caches.open(CACHE_NAME).then((cache) => {
+            console.log(
+              `Service Worker: Caching new response for ${event.request.url}`
+            );
             cache.put(event.request, response.clone());
           });
         }
@@ -126,6 +136,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
