17f93b3ec2129cfb9560f9921188c9c478ca71da
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-12-11 20:07:02 -0600

    modificacion del sistema de actualizacones y correccion de bugs

--- src/components/UpdateNotification.jsx
+++ src/components/UpdateNotification.jsx
@@ -1,11 +1,54 @@
+// import React from "react";
+// import { useUpdate } from "../contexts/UpdateContext";
+
+// function UpdateNotification() {
+//   const { updateAvailable, triggerUpdate, newVersion } = useUpdate(); // Accede al estado de la actualización y la nueva versión
+
+//   // Si no hay actualización disponible, no se muestra nada
+//   // if (!updateAvailable) return null;
+
+//   return (
+//     <div className="fixed bottom-0 left-0 right-0 bg-slate-800 text-white p-4 text-center">
+//       <div className="flex gap-2 justify-center">
+//         <h2 className="font-semibold text-lg text-gray-200">
+//           ¡Nueva versión disponible!
+//         </h2>
+//         {/* Muestra la nueva versión en lugar de la versión actual */}
+//         <p className="text-center text-gray-100 text-lg">
+//           <strong>{newVersion}</strong>
+//         </p>
+//       </div>
+
+//       <p className="text-gray-300">
+//         Hay una nueva actualización disponible. ¿Quieres actualizar ahora?
+//       </p>
+//       <div className="mt-2">
+//         <button
+//           onClick={triggerUpdate} // Al hacer clic en "Actualizar ahora", ejecuta la función para actualizar
+//           className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
+//         >
+//           Actualizar ahora
+//         </button>
+//         <button
+//           onClick={() => setUpdateAvailable(false);} // Recarga la página sin aplicar la actualización
+//           className="bg-gray-500 text-white px-4 py-2 rounded-md"
+//         >
+//           Más tarde
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default UpdateNotification;
+
+
 import React from "react";
 import { useUpdate } from "../contexts/UpdateContext";
 
 function UpdateNotification() {
-  const { updateAvailable, triggerUpdate, newVersion } = useUpdate(); // Accede al estado de la actualización y la nueva versión
-
-  // Si no hay actualización disponible, no se muestra nada
-  if (!updateAvailable) return null;
+  const { updateAvailable, triggerUpdate, newVersion, setUpdateAvailable } =
+    useUpdate();
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-slate-800 text-white p-4 text-center">
@@ -13,7 +56,6 @@ function UpdateNotification() {
         <h2 className="font-semibold text-lg text-gray-200">
           ¡Nueva versión disponible!
         </h2>
-        {/* Muestra la nueva versión en lugar de la versión actual */}
         <p className="text-center text-gray-100 text-lg">
           <strong>{newVersion}</strong>
         </p>
@@ -24,17 +66,17 @@ function UpdateNotification() {
       </p>
       <div className="mt-2">
         <button
-          onClick={triggerUpdate} // Al hacer clic en "Actualizar ahora", ejecuta la función para actualizar
+          onClick={triggerUpdate}
           className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2"
         >
           Actualizar ahora
         </button>
-        {/* <button
-          onClick={() => window.location.reload()} // Recarga la página sin aplicar la actualización
+        <button
+          onClick={() => setUpdateAvailable(false)}
           className="bg-gray-500 text-white px-4 py-2 rounded-md"
         >
           Más tarde
-        </button> */}
+        </button>
       </div>
     </div>
   );
--- src/contexts/UpdateContext.jsx
+++ src/contexts/UpdateContext.jsx
@@ -1,3 +1,60 @@
+// import { createContext, useContext, useState, useEffect } from "react";
+
+// const UpdateContext = createContext();
+
+// export const UpdateProvider = ({ children }) => {
+//   const [updateAvailable, setUpdateAvailable] = useState(false);
+//   const [newVersion, setNewVersion] = useState(null); // Para almacenar la nueva versión
+
+//   useEffect(() => {
+//     if ("serviceWorker" in navigator) {
+//       navigator.serviceWorker.ready.then((registration) => {
+//         registration.addEventListener("updatefound", () => {
+//           const newWorker = registration.installing;
+
+//           newWorker.addEventListener("statechange", () => {
+//             if (
+//               newWorker.state === "installed" &&
+//               navigator.serviceWorker.controller
+//             ) {
+//               // Una nueva versión está disponible
+//               setUpdateAvailable(true);
+//             }
+//           });
+//         });
+//       });
+
+//       navigator.serviceWorker.addEventListener("message", (event) => {
+//         if (event.data.type === "UPDATE_READY") {
+//           setUpdateAvailable(true);
+//           setNewVersion(event.data.newVersion); // Guardamos la nueva versión
+//         }
+//       });
+//     }
+//   }, []);
+
+//   // Función para manejar la actualización
+//   const triggerUpdate = () => {
+//     if (navigator.serviceWorker.controller) {
+//       navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
+//     }
+//     window.location.reload(); // Recarga la página una vez se aplica la actualización
+//   };
+
+//   return (
+//     <UpdateContext.Provider
+//       value={{ updateAvailable, triggerUpdate, newVersion }}
+//     >
+//       {children}
+//     </UpdateContext.Provider>
+//   );
+// };
+
+// export const useUpdate = () => useContext(UpdateContext);
+
+
+
+
 import { createContext, useContext, useState, useEffect } from "react";
 
 const UpdateContext = createContext();
@@ -5,6 +62,7 @@ const UpdateContext = createContext();
 export const UpdateProvider = ({ children }) => {
   const [updateAvailable, setUpdateAvailable] = useState(false);
   const [newVersion, setNewVersion] = useState(null); // Para almacenar la nueva versión
+  const [updateDecisionMade, setUpdateDecisionMade] = useState(false);
 
   useEffect(() => {
     if ("serviceWorker" in navigator) {
@@ -19,6 +77,11 @@ export const UpdateProvider = ({ children }) => {
             ) {
               // Una nueva versión está disponible
               setUpdateAvailable(true);
+              // Si el usuario no ha tomado una decisión, no se aplica la actualización automáticamente
+              if (!updateDecisionMade) {
+                // No se aplica la actualización automáticamente
+                return;
+              }
             }
           });
         });
@@ -31,7 +94,7 @@ export const UpdateProvider = ({ children }) => {
         }
       });
     }
-  }, []);
+  }, [updateDecisionMade]);
 
   // Función para manejar la actualización
   const triggerUpdate = () => {
@@ -43,7 +106,13 @@ export const UpdateProvider = ({ children }) => {
 
   return (
     <UpdateContext.Provider
-      value={{ updateAvailable, triggerUpdate, newVersion }}
+      value={{
+        updateAvailable,
+        triggerUpdate,
+        newVersion,
+        setUpdateAvailable,
+        setUpdateDecisionMade,
+      }}
     >
       {children}
     </UpdateContext.Provider>
