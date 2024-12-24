79f3a6ad497d76d35a2dd9db7e7844297c043b9a
Jhonatan Muñoz <jhonatanm255@hotmail.com>
2024-12-23 21:39:45 -0600

    up

--- src/components/UpdateNotification.jsx
+++ src/components/UpdateNotification.jsx
@@ -1,102 +1,20 @@
-// import React, { useState, useEffect } from "react";
-// import { useUpdate } from "../contexts/UpdateContext";
-// import { database } from "./firebaseConfig";
-// import { ref, set } from "firebase/database";
-
-// function UpdateNotification() {
-//   const { updateAvailable, triggerUpdate, newVersion, setUpdateAvailable } =
-//     useUpdate();
-
-//   // Estado para controlar la visibilidad del modal
-//   const [isVisible, setIsVisible] = useState(false);
-
-//   useEffect(() => {
-//     if (updateAvailable) setIsVisible(true);
-//   }, [updateAvailable]);
-
-//   if (!isVisible) return null;
-
-//   const handleLater = () => {
-//     // Cierra el modal
-//     setIsVisible(false);
-
-//     // Guardar la actualización pendiente en Firebase
-//     const updateRef = ref(database, "updates/pending");
-//     set(updateRef, { version: newVersion, status: "pending" })
-//       .then(() => console.log("Actualización pendiente registrada"))
-//       .catch((error) =>
-//         console.error("Error al guardar la actualización pendiente:", error)
-//       );
-
-//     // Actualizar el estado global
-//     setUpdateAvailable(false);
-//   };
-
-//   const handleUpdateNow = () => {
-//     triggerUpdate(); // Inicia la actualización
-//     setIsVisible(false); // Cierra el modal
-
-//     // Ya no es necesario limpiar la actualización pendiente en Firebase aquí,
-//     // ya que el triggerUpdate debe ocuparse de eso.
-//   };
-
-//   return (
-//     <div className="fixed bottom-0 left-0 right-0 bg-slate-800 text-white p-4 text-center shadow-lg animate-slide-up">
-//       <div className="flex gap-2 justify-center">
-//         <h2 className="font-semibold text-lg text-gray-200">
-//           ¡Nueva versión disponible!
-//         </h2>
-//         <p className="text-center text-gray-100 text-lg">
-//           <strong>{newVersion}</strong>
-//         </p>
-//       </div>
-
-//       <p className="text-gray-300">
-//         Hay una nueva actualización disponible. ¿Quieres actualizar ahora?
-//       </p>
-//       <div className="mt-2">
-//         <button
-//           onClick={handleUpdateNow}
-//           className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md mr-2"
-//         >
-//           Actualizar ahora
-//         </button>
-//         <button
-//           onClick={handleLater}
-//           className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md"
-//         >
-//           Más tarde
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default UpdateNotification;
-
-
-
-
-
-
 import React, { useState, useEffect } from "react";
 import { useUpdate } from "../contexts/UpdateContext";
 import { database } from "./firebaseConfig";
-import { ref, set, remove } from "firebase/database";
+import { ref, set } from "firebase/database";
 
 function UpdateNotification() {
   const { updateAvailable, triggerUpdate, newVersion, setUpdateAvailable } =
     useUpdate();
 
+  // Estado para controlar la visibilidad del modal
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    if (updateAvailable) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false); // Si no hay actualización disponible, cerramos el modal
-    }
-  }, [updateAvailable]); // El modal se actualizará cada vez que `updateAvailable` cambie
+    if (updateAvailable) setIsVisible(true);
+  }, [updateAvailable]);
+
+  if (!isVisible) return null;
 
   const handleLater = () => {
     // Cierra el modal
@@ -111,27 +29,16 @@ function UpdateNotification() {
       );
 
     // Actualizar el estado global
-    setUpdateAvailable(false); // Cambiar a 'false' para que el botón de "Update Pending" no aparezca
+    setUpdateAvailable(false);
   };
 
   const handleUpdateNow = () => {
-    // Inicia la actualización
-    triggerUpdate();
+    triggerUpdate(); // Inicia la actualización
+    setIsVisible(false); // Cierra el modal
 
-    // Eliminar la actualización pendiente en Firebase
-    const updateRef = ref(database, "updates/pending");
-    remove(updateRef)
-      .then(() => {
-        console.log("Actualización pendiente eliminada de Firebase.");
-        setUpdateAvailable(false); // Cambiar a 'false' para que el botón de "Update Pending" no aparezca
-      })
-      .catch((error) => console.error("Error al eliminar de Firebase:", error));
-
-    // Cerrar el modal
-    setIsVisible(false);
+    // Ya no es necesario limpiar la actualización pendiente en Firebase aquí,
+    // ya que el triggerUpdate debe ocuparse de eso.
   };
-
-  if (!isVisible) return null; // Si no debe ser visible, no renderizamos el modal
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-slate-800 text-white p-4 text-center shadow-lg animate-slide-up">
--- src/contexts/UpdateContext.jsx
+++ src/contexts/UpdateContext.jsx
@@ -1,130 +1,3 @@
-// import { createContext, useContext, useState, useEffect } from "react";
-// import { database } from "../components/firebaseConfig"; // Importa la configuración de Firebase
-// import { ref, set, remove, get } from "firebase/database"; // Métodos de Firebase
-
-// const UpdateContext = createContext();
-
-// export const UpdateProvider = ({ children }) => {
-//   const [updateAvailable, setUpdateAvailable] = useState(false);
-//   const [newVersion, setNewVersion] = useState(null);
-//   const [pendingUpdate, setPendingUpdate] = useState(false);
-
-//   useEffect(() => {
-//     // Revisamos si hay una actualización pendiente en Firebase
-//     const checkPendingUpdate = async () => {
-//       const updateRef = ref(database, "updates/pending");
-//       const snapshot = await get(updateRef);
-//       if (snapshot.exists()) {
-//         const updateData = snapshot.val();
-//         setPendingUpdate(true);
-//         setNewVersion(updateData.version);
-//       }
-//     };
-
-//     checkPendingUpdate();
-
-//     if ("serviceWorker" in navigator) {
-//       navigator.serviceWorker.ready.then((registration) => {
-//         // Detectar nuevos service workers instalados
-//         registration.addEventListener("updatefound", () => {
-//           const newWorker = registration.installing;
-//           newWorker.addEventListener("statechange", () => {
-//             if (
-//               newWorker.state === "installed" &&
-//               navigator.serviceWorker.controller
-//             ) {
-//               setUpdateAvailable(true);
-//               console.log("Nueva versión del Service Worker disponible.");
-//             } else if (newWorker.state === "redundant") {
-//               console.error("Service Worker instalación fallida (redundant).");
-//             }
-//           });
-//         });
-//       });
-
-//       // Manejar mensajes del Service Worker
-//       navigator.serviceWorker.addEventListener("message", (event) => {
-//         if (event.data.type === "UPDATE_READY") {
-//           setUpdateAvailable(true);
-//           setNewVersion(event.data.newVersion);
-
-//           // Guardar actualización pendiente en Firebase
-//           const updateRef = ref(database, "updates/pending");
-//           set(updateRef, {
-//             version: event.data.newVersion,
-//             status: "pending",
-//           })
-//             .then(() =>
-//               console.log("Actualización pendiente registrada en Firebase.")
-//             )
-//             .catch((error) =>
-//               console.error("Error al guardar en Firebase:", error)
-//             );
-//         }
-//       });
-//     }
-//   }, []);
-
-//   const triggerUpdate = () => {
-//     if (navigator.serviceWorker.controller) {
-//       navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
-//     }
-
-//     // Eliminar la actualización pendiente en Firebase
-//     const updateRef = ref(database, "updates/pending");
-//     remove(updateRef)
-//       .then(() => {
-//         console.log("Actualización pendiente eliminada de Firebase.");
-//         setPendingUpdate(false); // Actualizamos el estado de la UI
-//         setUpdateAvailable(false); // Ya no hay actualización disponible
-//       })
-//       .catch((error) => console.error("Error al eliminar de Firebase:", error));
-
-//     // Recargar la página para aplicar la nueva versión
-//     window.location.reload();
-//   };
-
-//   const dismissUpdate = () => {
-//     // Eliminar la actualización pendiente sin aplicar la actualización
-//     const updateRef = ref(database, "updates/pending");
-//     remove(updateRef)
-//       .then(() => {
-//         console.log("Actualización pendiente descartada.");
-//         setPendingUpdate(false); // Actualizamos el estado de la UI
-//         setUpdateAvailable(false); // Ya no hay actualización disponible
-//       })
-//       .catch((error) => console.error("Error al eliminar de Firebase:", error));
-//   };
-
-//   return (
-//     <UpdateContext.Provider
-//       value={{
-//         updateAvailable,
-//         triggerUpdate,
-//         dismissUpdate,
-//         newVersion,
-//         pendingUpdate,
-//         setUpdateAvailable,
-//         setPendingUpdate,
-//       }}
-//     >
-//       {children}
-//     </UpdateContext.Provider>
-//   );
-// };
-
-// export const useUpdate = () => useContext(UpdateContext);
-
-
-
-
-
-
-
-
-
-
-
 import { createContext, useContext, useState, useEffect } from "react";
 import { database } from "../components/firebaseConfig"; // Importa la configuración de Firebase
 import { ref, set, remove, get } from "firebase/database"; // Métodos de Firebase
@@ -192,7 +65,7 @@ export const UpdateProvider = ({ children }) => {
     }
   }, []);
 
-  const applyUpdate = () => {
+  const triggerUpdate = () => {
     if (navigator.serviceWorker.controller) {
       navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
     }
@@ -202,32 +75,13 @@ export const UpdateProvider = ({ children }) => {
     remove(updateRef)
       .then(() => {
         console.log("Actualización pendiente eliminada de Firebase.");
-        setPendingUpdate(false);
-        setUpdateAvailable(false);
-
-        // Recargar la página después de activar el nuevo Service Worker
-        navigator.serviceWorker.ready.then((registration) => {
-          if (registration.waiting) {
-            registration.waiting.postMessage({ type: "SKIP_WAITING" });
-          }
-
-          registration.update().then(() => {
-            console.log("Recargando la app para aplicar la actualización...");
-             Swal.fire({
-               title: "Actualizando!",
-               text: "La aplicación se reiniciará para aplicar la nueva actualización!",
-               icon: "success",
-               timer: 1000,
-             });
-            setTimeout(() => {
-              window.location.reload();
-            }, 1000);
-          });
-        });
+        setPendingUpdate(false); // Actualizamos el estado de la UI
+        setUpdateAvailable(false); // Ya no hay actualización disponible
       })
-      .catch((error) =>
-        console.error("Error al eliminar actualización de Firebase:", error)
-      );
+      .catch((error) => console.error("Error al eliminar de Firebase:", error));
+
+    // Recargar la página para aplicar la nueva versión
+    window.location.reload();
   };
 
   const dismissUpdate = () => {
@@ -236,8 +90,8 @@ export const UpdateProvider = ({ children }) => {
     remove(updateRef)
       .then(() => {
         console.log("Actualización pendiente descartada.");
-        setPendingUpdate(false);
-        setUpdateAvailable(false);
+        setPendingUpdate(false); // Actualizamos el estado de la UI
+        setUpdateAvailable(false); // Ya no hay actualización disponible
       })
       .catch((error) => console.error("Error al eliminar de Firebase:", error));
   };
@@ -246,7 +100,7 @@ export const UpdateProvider = ({ children }) => {
     <UpdateContext.Provider
       value={{
         updateAvailable,
-        applyUpdate,
+        triggerUpdate,
         dismissUpdate,
         newVersion,
         pendingUpdate,
@@ -260,3 +114,149 @@ export const UpdateProvider = ({ children }) => {
 };
 
 export const useUpdate = () => useContext(UpdateContext);
+
+
+
+
+
+
+
+
+
+
+
+// import { createContext, useContext, useState, useEffect } from "react";
+// import { database } from "../components/firebaseConfig"; // Importa la configuración de Firebase
+// import { ref, set, remove, get } from "firebase/database"; // Métodos de Firebase
+
+// const UpdateContext = createContext();
+
+// export const UpdateProvider = ({ children }) => {
+//   const [updateAvailable, setUpdateAvailable] = useState(false);
+//   const [newVersion, setNewVersion] = useState(null);
+//   const [pendingUpdate, setPendingUpdate] = useState(false);
+
+//   useEffect(() => {
+//     // Revisamos si hay una actualización pendiente en Firebase
+//     const checkPendingUpdate = async () => {
+//       const updateRef = ref(database, "updates/pending");
+//       const snapshot = await get(updateRef);
+//       if (snapshot.exists()) {
+//         const updateData = snapshot.val();
+//         setPendingUpdate(true);
+//         setNewVersion(updateData.version);
+//       }
+//     };
+
+//     checkPendingUpdate();
+
+//     if ("serviceWorker" in navigator) {
+//       navigator.serviceWorker.ready.then((registration) => {
+//         // Detectar nuevos service workers instalados
+//         registration.addEventListener("updatefound", () => {
+//           const newWorker = registration.installing;
+//           newWorker.addEventListener("statechange", () => {
+//             if (
+//               newWorker.state === "installed" &&
+//               navigator.serviceWorker.controller
+//             ) {
+//               setUpdateAvailable(true);
+//               console.log("Nueva versión del Service Worker disponible.");
+//             } else if (newWorker.state === "redundant") {
+//               console.error("Service Worker instalación fallida (redundant).");
+//             }
+//           });
+//         });
+//       });
+
+//       // Manejar mensajes del Service Worker
+//       navigator.serviceWorker.addEventListener("message", (event) => {
+//         if (event.data.type === "UPDATE_READY") {
+//           setUpdateAvailable(true);
+//           setNewVersion(event.data.newVersion);
+
+//           // Guardar actualización pendiente en Firebase
+//           const updateRef = ref(database, "updates/pending");
+//           set(updateRef, {
+//             version: event.data.newVersion,
+//             status: "pending",
+//           })
+//             .then(() =>
+//               console.log("Actualización pendiente registrada en Firebase.")
+//             )
+//             .catch((error) =>
+//               console.error("Error al guardar en Firebase:", error)
+//             );
+//         }
+//       });
+//     }
+//   }, []);
+
+//   const applyUpdate = () => {
+//     if (navigator.serviceWorker.controller) {
+//       navigator.serviceWorker.controller.postMessage({ type: "SKIP_WAITING" });
+//     }
+
+//     // Eliminar la actualización pendiente en Firebase
+//     const updateRef = ref(database, "updates/pending");
+//     remove(updateRef)
+//       .then(() => {
+//         console.log("Actualización pendiente eliminada de Firebase.");
+//         setPendingUpdate(false);
+//         setUpdateAvailable(false);
+
+//         // Recargar la página después de activar el nuevo Service Worker
+//         navigator.serviceWorker.ready.then((registration) => {
+//           if (registration.waiting) {
+//             registration.waiting.postMessage({ type: "SKIP_WAITING" });
+//           }
+
+//           registration.update().then(() => {
+//             console.log("Recargando la app para aplicar la actualización...");
+//              Swal.fire({
+//                title: "Actualizando!",
+//                text: "La aplicación se reiniciará para aplicar la nueva actualización!",
+//                icon: "success",
+//                timer: 1000,
+//              });
+//             setTimeout(() => {
+//               window.location.reload();
+//             }, 1000);
+//           });
+//         });
+//       })
+//       .catch((error) =>
+//         console.error("Error al eliminar actualización de Firebase:", error)
+//       );
+//   };
+
+//   const dismissUpdate = () => {
+//     // Eliminar la actualización pendiente sin aplicar la actualización
+//     const updateRef = ref(database, "updates/pending");
+//     remove(updateRef)
+//       .then(() => {
+//         console.log("Actualización pendiente descartada.");
+//         setPendingUpdate(false);
+//         setUpdateAvailable(false);
+//       })
+//       .catch((error) => console.error("Error al eliminar de Firebase:", error));
+//   };
+
+//   return (
+//     <UpdateContext.Provider
+//       value={{
+//         updateAvailable,
+//         applyUpdate,
+//         dismissUpdate,
+//         newVersion,
+//         pendingUpdate,
+//         setUpdateAvailable,
+//         setPendingUpdate,
+//       }}
+//     >
+//       {children}
+//     </UpdateContext.Provider>
+//   );
+// };
+
+// export const useUpdate = () => useContext(UpdateContext);
